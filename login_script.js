1671f9d1dc23b5508d3d4b5bf500f6d9091962d9
Thoungsopheak <157412348+Thoungsopheak@users.noreply.github.com>
2024-03-04 11:47:10 -0500

    Add files via upload

---
+++ login_script.js
@@ -0,0 +1,15 @@
+function handleFormSubmission() {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    var trackingNumber = document.getElementsByName('trackingNumber')[0].value;
+    var placeholderMessage = '';
+    // Check if the tracking number is "12345678"
+    if (trackingNumber === '12345678') {
+        // Redirect the user to "main.html"
+        window.location.href = 'main.html';
+    } else {
+       // Perform other actions or provide feedback for incorrect tracking numbers
+       alert('Invalid tracking number. Please try again.');
+    }
+}
